4fc5e140b144576eb97e398ce25e0cddae4be891
Frank <frankwangma@gmail.com>
2020-07-29 15:57:38 +1200

    Added a script to automatically space out key overlays

--- Assets/Scripts/Control/LoadPianoOverlay.cs
+++ Assets/Scripts/Control/LoadPianoOverlay.cs
@@ -7,7 +7,22 @@ public class LoadPianoOverlay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject parent = GameObject.Find("PianoOverlay");
+        if (parent.transform.childCount != 0)
+            {
+                
+                RectTransform prevChildRect = null; 
+                foreach(Transform child in parent.transform)
+                {
+                   RectTransform rect = child.GetComponent<RectTransform>();
+                   if(prevChildRect != null) 
+                   {
+                       rect.localPosition = prevChildRect.localPosition - new Vector3(93,0,0);
+                   }
+
+                   prevChildRect = rect;
+                }
+            }
     }
 
     // Update is called once per frame
