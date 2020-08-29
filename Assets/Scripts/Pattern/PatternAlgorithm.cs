25b3bf46be387c674209d5ddea89d2cfe8d4a9cc
Thomas <thomaszhu.nz@gmail.com>
2020-08-29 17:10:30 +1200

    Add comments

--- Assets/Scripts/Pattern/PatternAlgorithm.cs
+++ Assets/Scripts/Pattern/PatternAlgorithm.cs
@@ -9,6 +9,19 @@ namespace Pattern
 {
     public class PatternAlgorithm
     {
+        /**
+Hashset - add notes up to three
+
+If duplicates are attempted to be added, stored the duplicate in a temporary list
+
+If a NEW note is found create a new hashset
+
+If the hashset new hashset only has TWO notes, add the MOST RECENT duplicate to the new hashset
+
+if the hashset only has ONE note, check if its is a 7th
+
+if is it NOT a 7th, and there are TWO duplicate notes or greater, we add the most two recent, and check if its a pattern
+        **/
         private List<Note> _notes = new List<Note>();
 
         private static NoteDatabase instance = new NoteDatabase();
